b094fe2d9702d1f3ad782815a1087e3dd6a9092f
Sylvia Chebet <sylviachebet03@gmail.com>
2024-07-08 00:59:33 +0300

    delete commented lines on about us page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,19 +1,3 @@
-// import React from 'react';
-
-// const AboutUs = () => {
-//   return (
-//     <div className="container mx-auto px-4 py-8">
-//       <h1 className="text-3xl font-bold mb-4">About Us</h1>
-//       <p className="text-gray-700 leading-relaxed">
-//       Welcome to Medimart, your trusted partner in health and wellness. At Medimart, we are committed to providing convenient access to high-quality medications and healthcare products, delivered right to your doorstep. Our mission is to empower individuals and families to take control of their health by offering a wide range of pharmaceuticals, wellness essentials, and personalized care solutions. Whether you're managing a chronic condition or simply looking for everyday health products, Medimart is here to support you every step of the way. Trust, reliability, and customer satisfaction are at the heart of what we do. Join us in our journey towards better health and a brighter future with Medimart.
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default AboutUs;
-
-
 
 import React from 'react';
 import { Parallax } from 'react-parallax';
