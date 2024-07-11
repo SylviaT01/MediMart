9d91bbbee1012042a8790fbf99a391eb26794680
Sylvia Chebet <sylviachebet03@gmail.com>
2024-07-12 00:47:14 +0300

    install gunicorn

--- server/run.py
+++ server/run.py
@@ -1,7 +1,15 @@
+# from app import create_app
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
+
 from app import create_app
+import os
+
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
 
