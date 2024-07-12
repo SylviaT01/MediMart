1b51e1d709366bdfa1294acad1303bac35e0e639
Sylvia Chebet <sylviachebet03@gmail.com>
2024-07-12 06:41:12 +0300

    edit run.py, create Procfile and edit requirements.txt

--- server/run.py
+++ server/run.py
@@ -1,7 +1,10 @@
+import os
 from app import create_app
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
     
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
